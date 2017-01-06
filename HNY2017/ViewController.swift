46c8826b589b0a0baaa470839c3debcf4b3562fb
Akshay Thakare <5083479@wtc-5083479-mba.local>
2017-01-05 20:45:25 -0600

    Added viewcontroller logic and images

--- HNY2017/ViewController.swift
+++ HNY2017/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    
+    
+    @IBOutlet weak var titleImage: UIImageView!
+    
+    
+    @IBOutlet weak var welcomeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Happy New Year 2017")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +28,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomeClicked(_ sender: Any) {
+        background.isHidden = false;
+        titleImage.isHidden = false;
+        welcomeButton.isHidden = true;
+    }
 
 }
 
